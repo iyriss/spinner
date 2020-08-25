6bee92450fc0d71cd71bf57501347a2c0087a12f
Iyris Vigil <iyrisvigil@gmail.com>
2020-08-25 19:38:18 +0000

    spinner animation

---
+++ spinner1.js
@@ -0,0 +1,38 @@
+process.stdout.write('hello from spinner1.js... \rhey\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);    
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\  ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|  ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r\\  ');
+}, 1400);
+
+setTimeout(() => {
+  process.stdout.write('\r|  ');
+}, 1600);
+
